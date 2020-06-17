be02ff4e19c452e5c6f9f92e540a174a9de459b4
FrancoFantomius <56490563+FrancoFantomius@users.noreply.github.com>
2020-06-17 13:37:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.errorhandler(404)
-def pnf(error):
-    return render_template("404.html"), 404
-
 if __name__=="__main__":
     app.run()
